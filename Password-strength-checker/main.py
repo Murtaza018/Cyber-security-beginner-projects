47601509d0ff08f36eef11c77c53f63b38e269dd
Murtaza <murtazaansari242@gmail.com>
2025-05-13 23:41:45 +0500

    4th commit

--- Password-strength-checker/main.py
+++ Password-strength-checker/main.py
@@ -6,10 +6,23 @@ if length < 10:
     print("Password is too short")
 if length> 25:
     print("Password is too long")  
-char_list=[]    
+char_list=[]
+uppercase=0    
+lowercase=0    
+digit=0
+special=0    
 for char in password:
     if char not in char_list:
         char_list.append(char)
+    if char.isupper():
+        uppercase+=1    
+    elif char.islower():
+        lowercase+=1    
+    elif char.isdigit():
+        digit+=1   
+    else:
+        special+=1
+             
 char_list_length=len(char_list)        
 print("char_list length:",char_list_length)
 if char_list_length < length/2:
