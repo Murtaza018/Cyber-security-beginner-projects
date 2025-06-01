bda20bb50660ce8cbef3760c1a281f167d19ea37
Murtaza <murtazaansari242@gmail.com>
2025-06-01 23:29:15 +0500

    added functionality to select scan type(tcp,udp or both)

--- Port-Scanner/main.py
+++ Port-Scanner/main.py
@@ -56,6 +56,15 @@ def write_results_to_json(filename,open_ports,closed_ports, filtered_ports, unfi
 
 ip_domain=input("Enter IP or Domain:")
 print(ip_domain)
+print("Choose Scan Type (Wrong option number will automatically choose default)")
+print("1-TCP (Default)")
+print("2-UDP")
+print("3-Both TCP and UDP")
+option=input("Enter option number:")
+option = option.strip()
+if option not in [1,2,3]:
+    option = 1
+
 
 common_ports={
 21 :["FTP",1],
@@ -73,43 +82,49 @@ print(common_ports)
 
 scanner=nmap.PortScanner()
 ports = ",".join(str(port) for port in common_ports.keys())
-state = scanner.scan(hosts=ip_domain, arguments=f"-sS -sU -p T:{ports},U:{ports}")
+if(option==3):
+    state = scanner.scan(hosts=ip_domain, arguments=f"-sS -sU -p T:{ports},U:{ports}")
+elif(option==2):
+    state = scanner.scan(hosts=ip_domain, arguments=f"-sU -p U:{ports}")
+else:
+    state = scanner.scan(hosts=ip_domain, arguments=f"-sS -p T:{ports}")
 open_ports={}
 closed_ports={}
 filtered_ports={}
 unfiltered_ports={}
 open_filtered_ports={}
 closed_filtered_ports={}
-for port in scanner[ip_domain]['tcp']:
-    if scanner[ip_domain]['tcp'][port]['state'] == 'open':
-        open_ports[port]=[scanner[ip_domain]['tcp'][port]['name'],'tcp']  
-    elif scanner[ip_domain]['tcp'][port]['state'] == 'closed':    
-        closed_ports[port]=[scanner[ip_domain]['tcp'][port]['name'],'tcp']
-    elif scanner[ip_domain]['tcp'][port]['state'] == 'filtered':
-        filtered_ports[port]=[scanner[ip_domain]['tcp'][port]['name'],'tcp']
-    elif scanner[ip_domain]['tcp'][port]['state'] == 'unfiltered':
-        unfiltered_ports[port]=[scanner[ip_domain]['tcp'][port]['name'],'tcp']
-    elif scanner[ip_domain]['tcp'][port]['state'] == 'open|filtered':
-        open_filtered_ports[port]=[scanner[ip_domain]['tcp'][port]['name'],'tcp']
-    elif scanner[ip_domain]['tcp'][port]['state'] == 'closed|filtered':
-        closed_filtered_ports[port]=[scanner[ip_domain]['tcp'][port]['name'],'tcp']
+if option !=2:
+    for port in scanner[ip_domain]['tcp']:
+        if scanner[ip_domain]['tcp'][port]['state'] == 'open':
+            open_ports[port]=[scanner[ip_domain]['tcp'][port]['name'],'tcp']  
+        elif scanner[ip_domain]['tcp'][port]['state'] == 'closed':    
+            closed_ports[port]=[scanner[ip_domain]['tcp'][port]['name'],'tcp']
+        elif scanner[ip_domain]['tcp'][port]['state'] == 'filtered':
+            filtered_ports[port]=[scanner[ip_domain]['tcp'][port]['name'],'tcp']
+        elif scanner[ip_domain]['tcp'][port]['state'] == 'unfiltered':
+            unfiltered_ports[port]=[scanner[ip_domain]['tcp'][port]['name'],'tcp']
+        elif scanner[ip_domain]['tcp'][port]['state'] == 'open|filtered':
+            open_filtered_ports[port]=[scanner[ip_domain]['tcp'][port]['name'],'tcp']
+        elif scanner[ip_domain]['tcp'][port]['state'] == 'closed|filtered':
+            closed_filtered_ports[port]=[scanner[ip_domain]['tcp'][port]['name'],'tcp']
 
-
-for port in scanner[ip_domain]['udp']:
-    if scanner[ip_domain]['udp'][port]['state'] == 'open':
-        open_ports[port]=[scanner[ip_domain]['udp'][port]['name'],'udp']  
-    elif scanner[ip_domain]['udp'][port]['state'] == 'closed':    
-        closed_ports[port]=[scanner[ip_domain]['udp'][port]['name'],'udp']
-    elif scanner[ip_domain]['udp'][port]['state'] == 'filtered':
-        filtered_ports[port]=[scanner[ip_domain]['udp'][port]['name'],'udp']
-    elif scanner[ip_domain]['udp'][port]['state'] == 'unfiltered':
-        unfiltered_ports[port]=[scanner[ip_domain]['udp'][port]['name'],'udp']
-    elif scanner[ip_domain]['udp'][port]['state'] == 'open|filtered':
-        open_filtered_ports[port]=[scanner[ip_domain]['udp'][port]['name'],'udp']
-    elif scanner[ip_domain]['udp'][port]['state'] == 'closed|filtered':
-        closed_filtered_ports[port]=[scanner[ip_domain]['udp'][port]['name'],'udp']
+if option ==2 or option==3:
+    for port in scanner[ip_domain]['udp']:
+        if scanner[ip_domain]['udp'][port]['state'] == 'open':
+            open_ports[port]=[scanner[ip_domain]['udp'][port]['name'],'udp']  
+        elif scanner[ip_domain]['udp'][port]['state'] == 'closed':    
+            closed_ports[port]=[scanner[ip_domain]['udp'][port]['name'],'udp']
+        elif scanner[ip_domain]['udp'][port]['state'] == 'filtered':
+            filtered_ports[port]=[scanner[ip_domain]['udp'][port]['name'],'udp']
+        elif scanner[ip_domain]['udp'][port]['state'] == 'unfiltered':
+            unfiltered_ports[port]=[scanner[ip_domain]['udp'][port]['name'],'udp']
+        elif scanner[ip_domain]['udp'][port]['state'] == 'open|filtered':
+            open_filtered_ports[port]=[scanner[ip_domain]['udp'][port]['name'],'udp']
+        elif scanner[ip_domain]['udp'][port]['state'] == 'closed|filtered':
+            closed_filtered_ports[port]=[scanner[ip_domain]['udp'][port]['name'],'udp']
      
-
+print(f"Scan type selected: {'TCP only' if option == '1' else 'UDP only' if option == '2' else 'Both TCP and UDP'}")
 print_ports("Open Ports", open_ports)
 print_ports("Closed Ports", closed_ports)
 print_ports("Filtered Ports", filtered_ports)
