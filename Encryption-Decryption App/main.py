8ec81633c0ae65832bcbb993aee47b518b3de7ba
Murtaza <murtazaansari242@gmail.com>
2025-06-16 21:46:38 +0500

    encryption/decryption app ongoing

--- Encryption-Decryption App/main.py
+++ Encryption-Decryption App/main.py
@@ -1,1 +1,17 @@
-print("Encryption Decryption App started")
+def selectType():
+    opt=input("Enter Type:Encryption(1)/Decryption(2):")
+    if(opt==1 or opt==2):
+        return opt
+    else:
+        print("Invalid Option!")    
+    return -1
+
+
+msg=input("Enter message:")
+opt=0
+while opt!=1 or opt!=2:
+    opt=selectType()
+if opt==1:
+    print("Encryption!")    
+if opt==2:
+    print("Decryption!")    
