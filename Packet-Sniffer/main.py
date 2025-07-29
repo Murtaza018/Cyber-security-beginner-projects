e7357805f8458a415c38d38962b11505a48ffd17
Murtaza <murtazaansari242@gmail.com>
2025-07-30 00:00:11 +0500

    adding filters

--- Packet-Sniffer/main.py
+++ Packet-Sniffer/main.py
@@ -6,3 +6,14 @@ def packet_callback(packet):
 
 sniff(prn=packet_callback, count=0)
 sniff(filter="tcp port 80", prn=packet_callback, count=0)
+
+
+print("Select a filter:")
+print("1-TCP")
+print("2-UDP")
+print("3-ICMP")
+print("4-Certain Port")
+print("5-Certain IP")
+print("6-All IP packets")
+print("7-ARP packets")
+print("8-IPv6 Packets")
